0afdad0f233be32297a5b606e0e8f18371e49e5e
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2025-12-31 06:08:55 +0100

    refactor: simplify Badge component return statement for improved readability

--- src/components/icon/badge.tsx
+++ src/components/icon/badge.tsx
@@ -9,30 +9,28 @@ export const Badge: FC<ExtendedSvg> = ({
     "class:list": class_list,
     className,
     ...props
-}) => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg"
-             width="24"
-             height="24"
-             viewBox="0 0 24 24"
-             fill="none"
-             stroke="currentColor"
-             strokeWidth="2"
-             strokeLinecap="round"
-             strokeLinejoin="round"
-             className={ clsx([
-                 "icon icon-tabler icons-tabler-outline icon-tabler-arrow-badge-right",
-                 className,
-                 classes,
-                 class_list,
-             ]) }
-             { ...props }
-        >
-            <path stroke="none"
-                  d="M0 0h24v24H0z"
-                  fill="none"
-            />
-            <path d="M13 7h-6l4 5l-4 5h6l4 -5z" />
-        </svg>
-    );
-};
+}) => (
+    <svg xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={ clsx([
+            `icon icon-tabler icons-tabler-outline icon-tabler-arrow-badge-right`,
+            className,
+            classes,
+            class_list,
+        ]) }
+        { ...props }
+    >
+        <path stroke="none"
+            d="M0 0h24v24H0z"
+            fill="none"
+        />
+        <path d="M13 7h-6l4 5l-4 5h6l4 -5z" />
+    </svg>
+);
