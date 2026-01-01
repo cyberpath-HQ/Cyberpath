ab2da16bd73d09106f690ccb76ff078749af497c
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-01-01 16:35:56 +0100

    fix: replace tailwind integration with tailwindcss in vite for improved configuration

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import { defineConfig } from "astro/config";
 import expressiveCode from 'astro-expressive-code';
@@ -19,9 +19,6 @@ export default defineConfig({
     base:          `/`,
     trailingSlash: `ignore`,
     integrations:  [
-        tailwind({
-            nesting: true,
-        }),
         react(),
         sitemap({
             filter: (page) => !(
@@ -78,4 +75,11 @@ export default defineConfig({
         prefetchAll:     true,
     },
     scopedStyleStrategy: `class`,
+    vite:                {
+        plugins: [
+            tailwindcss({
+                optimize: true,
+            }),
+        ],
+    },
 });
