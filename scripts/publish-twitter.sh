7ad08e47a53fc5fd26fa1ec28335e1410fd34e63
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-01-02 18:33:40 +0100

    fix: add wait_on_rate_limit option to Twitter API client for better rate limit handling

--- scripts/publish-twitter.sh
+++ scripts/publish-twitter.sh
@@ -137,7 +137,8 @@ client = tweepy.Client(
     consumer_key=os.environ['TWITTER_API_KEY'],
     consumer_secret=os.environ['TWITTER_API_SECRET'],
     access_token=os.environ['TWITTER_ACCESS_TOKEN'],
-    access_token_secret=os.environ['TWITTER_ACCESS_SECRET']
+    access_token_secret=os.environ['TWITTER_ACCESS_SECRET'],
+    wait_on_rate_limit=True
 )
 
 # Post tweet
